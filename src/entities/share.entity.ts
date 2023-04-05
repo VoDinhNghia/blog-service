ae4c8b08d0d83296af68429c058dad531ff6fed3
VoDinhNghia <vodinhnghia85@gmail.com>
2023-04-05 20:29:44 +0700

    create entity share and like

--- src/entities/share.entity.ts
+++ src/entities/share.entity.ts
@@ -1,5 +1,34 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, ManyToMany, ManyToOne, OneToMany } from 'typeorm';
 import { EntityBasic } from './base.entity';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { Likes } from './like.entity';
+import { Posts } from './post.entity';
+import { User } from './user.entity';
 
 @Entity('shares')
-export class Shares extends EntityBasic {}
+export class Shares extends EntityBasic {
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  userId?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  postId?: string;
+
+  @IsBoolean()
+  @Column({ default: false })
+  privateMode?: boolean;
+
+  @OneToMany(() => Likes, (like) => like.share, {
+    cascade: ['soft-remove', 'recover'],
+  })
+  likes?: Likes[];
+
+  @ManyToOne(() => Posts, (post) => post.shares)
+  post?: Posts;
+
+  @ManyToMany(() => User, (user) => user.shares)
+  users?: User[];
+}
