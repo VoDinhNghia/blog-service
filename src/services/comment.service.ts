36f769a281b6b286b80e66e7c5f0772db2af1aba
VoDinhNghia <vodinhnghia85@gmail.com>
2023-04-15 16:43:51 +0700

    write api update and delete comment

--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -4,7 +4,8 @@ import { Comments } from '../entities/comment.entity';
 import { Response } from 'express';
 import { Posts } from '../entities/post.entity';
 import { CommonException } from '../exceptions/exceptions.common-error';
-import { postMsg } from '../constants/constants.message-response';
+import { commentMsg, postMsg } from '../constants/constants.message-response';
+import { Equal } from 'typeorm';
 
 export class CommentService {
   private commentRepository = AppDataSource.getRepository(Comments);
@@ -31,5 +32,51 @@ export class CommentService {
     };
     const result = await this.commentRepository.save(commentDto);
     return result;
+  }
+
+  async updateComment(
+    res: Response,
+    id: string,
+    body: IcreateComment,
+    userId: string
+  ): Promise<void | object> {
+    const comment = await this.commentRepository.findOne({
+      where: {
+        id: Equal(id),
+      },
+    });
+    if (!comment) {
+      return new CommonException(res, 404, commentMsg.notFound);
+    }
+    if (String(userId) !== String(comment.userId)) {
+      return new CommonException(res, 403, commentMsg.notPermission);
+    }
+    await this.commentRepository.update(id, body);
+  }
+
+  async deleteComment(
+    res: Response,
+    id: string,
+    userId: string
+  ): Promise<void | object> {
+    const comment = await this.commentRepository.findOne({
+      where: {
+        id: Equal(id),
+      },
+      relations: {
+        post: true,
+      },
+    });
+    if (!comment) {
+      return new CommonException(res, 404, commentMsg.notFound);
+    }
+    if (
+      String(userId) === String(comment.userId) ||
+      String(userId) === String(comment.post?.userId)
+    ) {
+      await this.commentRepository.delete(id);
+    } else {
+      return new CommonException(res, 403, commentMsg.notPermission);
+    }
   }
 }
